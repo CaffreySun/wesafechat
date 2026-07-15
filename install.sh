b197c18ec1111f02d249f48830343593aa5a61c0
Caffrey <sunhongwei0913@163.com>
2026-07-15 14:27:08 +0800

    feat: add app icon generation from logo.png

--- install.sh
+++ install.sh
@@ -16,6 +16,24 @@ swiftc main.swift -o "${CONTENTS}/${APP_NAME}" \
 echo "==> 复制 Info.plist..."
 cp Info.plist "${APP_BUNDLE}/Contents/Info.plist"
 
+echo "==> 生成应用图标..."
+ICONSET="AppIcon.iconset"
+mkdir -p "$ICONSET"
+sips -z 16 16   logo.png --out "${ICONSET}/icon_16x16.png"
+sips -z 32 32   logo.png --out "${ICONSET}/icon_16x16@2x.png"
+sips -z 32 32   logo.png --out "${ICONSET}/icon_32x32.png"
+sips -z 64 64   logo.png --out "${ICONSET}/icon_32x32@2x.png"
+sips -z 128 128 logo.png --out "${ICONSET}/icon_128x128.png"
+sips -z 256 256 logo.png --out "${ICONSET}/icon_128x128@2x.png"
+sips -z 256 256 logo.png --out "${ICONSET}/icon_256x256.png"
+sips -z 512 512 logo.png --out "${ICONSET}/icon_256x256@2x.png"
+sips -z 512 512 logo.png --out "${ICONSET}/icon_512x512.png"
+sips -z 1024 1024 logo.png --out "${ICONSET}/icon_512x512@2x.png"
+iconutil -c icns "$ICONSET" -o "AppIcon.icns"
+mkdir -p "${APP_BUNDLE}/Contents/Resources"
+cp AppIcon.icns "${APP_BUNDLE}/Contents/Resources/AppIcon.icns"
+rm -rf "$ICONSET" AppIcon.icns
+
 echo "==> 构建完成: ${APP_BUNDLE}"
 
 echo ""
